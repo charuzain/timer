1efb49824fabcbf8bc611111df594e1b0d019f5c
Charu Jain <charuabhizain@gmail.com>
2021-09-27 01:46:28 -0400

    Update timer1.js

--- timer1.js
+++ timer1.js
@@ -3,10 +3,9 @@ const timer = process.argv.slice(2).sort();
 
 let alarmTimer = timer.map(Number);
 
-//console.log(alarmTimer);
 for(let i = 0;i<alarmTimer.length;i++){
   setTimeout(()=>{
-    //console.log("hi")
+   
     process.stdout.write('\x07');
   },alarmTimer[i]*1000)
 }
